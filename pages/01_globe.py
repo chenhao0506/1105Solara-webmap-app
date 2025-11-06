e40e320e6495a7df897e1757029e63e0e33e569e
chenhao0506 <dannyfann0506@gmail.com>
2025-11-06 01:20:11 +0000

    modified:   pages/01_globe.py

--- pages/01_globe.py
+++ pages/01_globe.py
@@ -2,26 +2,23 @@ import solara
 from anymap import MapLibreMap
 
 
-class Map(MapLibreMap):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        super().add_basemap("CartoDB.DarkMatter")
+def create_map():
+    m = MapLibreMap()
+    m.add_basemap("CartoDB.DarkMatter")
 
-        # 加入 GitHub 上的 GeoJSON 檔案
-        routes_url = "https://raw.githubusercontent.com/chenhao0506/1105Solara-webmap-app/main/routes.geojson"
-        stations_url = "https://raw.githubusercontent.com/chenhao0506/1105Solara-webmap-app/main/stations.geojson"
+    # 加入 GitHub 上的 GeoJSON 檔案
+    routes_url = "https://raw.githubusercontent.com/chenhao0506/1105Solara-webmap-app/main/routes.geojson"
+    stations_url = "https://raw.githubusercontent.com/chenhao0506/1105Solara-webmap-app/main/stations.geojson"
 
-        # 加入圖層
-        super().add_geojson(routes_url, name="Routes")
-        super().add_geojson(stations_url, name="Stations")
+    # 加入圖層
+    m.add_geojson(routes_url, name="Routes")
+    m.add_geojson(stations_url, name="Stations")
 
-        super().add_layer_control(collapsed=False)
+    m.add_layer_control(collapsed=False)
+    return m
 
 
 @solara.component
 def Page():
     with solara.Card():
-        Map().element()
-
-
-Page()
+        create_map().element()
