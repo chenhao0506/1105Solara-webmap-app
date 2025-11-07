d9a4c4927bb08178b3286d6261864bb8978c2c34
chenhao0506 <dannyfann0506@gmail.com>
2025-11-07 06:53:02 +0000

    modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -5,11 +5,8 @@ import solara
 def Page():
     with solara.Column(align="center"):
         markdown = """
-        ## 3D Mapping with Leafmap and MapLibre
-        This is a Solara template for a 3D mapping application using Leafmap and MapLibre. Click on the menu above to see the different examples.
-        <br>
-        Source code: <https://github.com/opengeos/solara-maplibre>
-        ![image](https://github.com/user-attachments/assets/efc9e43b-99c0-40b4-af08-4971e8b96919)
+        ## Solara 台北 GIS 儀表板
+        歡迎來到宸晧的 Solara 台北 GIS 儀表板!
         """
 
         solara.Markdown(markdown)
